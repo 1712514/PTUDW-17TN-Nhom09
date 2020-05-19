4cd865c7a3f6bb6b8e72f72ba4fceabc5676868b
1712694 <1712694@student.hcmus.edu.vn>
2020-05-19 15:56:51 +0700

    Add pseudo data to acheivement

--- front-end/js/Sprofile.js
+++ front-end/js/Sprofile.js
@@ -21,15 +21,24 @@ createRow = (rowdata) => {
     var tr = document.createElement("tr");
     tr.setAttribute("class", "font-white");
     var th = document.createElement("th");
+    th.setAttribute("scope", "row");
+    th.textContent = rowdata[0];
+    var td = document.createElement("td");
+    td.textContent = rowdata[1];
+    tr.appendChild(th);
+    tr.appendChild(td);
+    return tr;
 }
 fillAcheivement = () => {
     var data = getAchievement();
     var body = document.getElementById("achiebody");
-
-    
+    Object.keys(data).forEach(function(key){
+        body.appendChild(createRow([key, data[key]]));
+    })
 }
 
 document.getElementById("avatar").setAttribute("src", getAvatarURI());
 document.getElementById("nameinfo").innerHTML += getFullName();
 document.getElementById("emailinfo").innerHTML += getEmail();
 document.getElementById("passwordinfo").innerHTML += getPassword();
+fillAcheivement();
