4bdb7f0efb63c58ecdbf8e2ffcff8a5559879f8e
Rayl99 <1712504@student.hcmus.edu.vn>
2020-05-30 22:24:11 +0700

    Complete Ssearchtest and Ssearchreview

--- front-end/js/Ssearchtest.js
+++ front-end/js/Ssearchtest.js
@@ -1,4 +1,54 @@
+var tests =[
+    {title:"Đề thi thử THPT quốc gia 2020 môn Tiếng anh", id:"TA123V",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Toán", id:"TO115D",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Hóa học",id:"HO343K",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Vật lý",id:"LI234D",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Sinh học",id:"SI643V",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Địa lý",id:"DI296D",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn Lịch sử",id:"SU342V",ref:""},
+    {title:"Đề thi thử THPT quốc gia 2020 môn GDCD",id:"GD2364K",ref:""},
+    {title:"Đề thi thử THPT quốc gia tỉnh Bình Phước môn Toán",id:"TO5434D",ref:""},
+    {title:"Đề thi thử THPT quốc gia tỉnh Bình Phước môn Sinh học",id:"SI432V",ref:""},
+    {title:"Đề thi thử THPT quốc gia tỉnh Đăk Lăk Sinh học",id:"SI346K",ref:""},
+    {title:"Đề thi thử THPT quốc gia tỉnh Bình Phước môn Vật lý",id:"SI341D",ref:""},
+    {title:"Đề thi thử THPT quốc gia tỉnh Bình Phước môn Địa lý",id:"DI231K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bình Phước môn Tiếng anh",id:"TA5343V",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bình Phước môn GDCD",id:"GD322D",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Đăk Lăk môn Vật lý",id:"LI988K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Đăk Lăk môn Tiếng anh",id:"TA959V",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Đăk Lăk môn Địa lý",id:"LI3434D",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Lâm Đồng môn GDCD",id:"GD953K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bến Tre môn Tiếng anh",id:"TA484D",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Lâm Đồng môn Tiếng anh",id:"TA3432K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Lâm Đồng môn Vật lý",id:"LI591D",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Lâm Đồng môn Địa lý",id:"DI539K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bến Tre môn Sinh học",id:"SI3234V",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bến Tre môn  Vật lý",id:"LI34521D",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bến Tre môn Sinh học",id:"SI342754K",ref:""},
+    {title:"Đề thi thử thpt quốc gia tỉnh Bến Tre môn Toán",id:"TO24341D",ref:""}    
+];
+
+
+
 $(document).ready(function(){
+    //set data into button
+    for(var t = 0;t<tests.length;t++){
+        $("<button/>",{
+            "type":"button",
+            "class":"col-2 btn testBtn",
+            "id":tests[t].id,
+            "data-toggle":"modal",
+            "data-target":"#myModal",
+            text:tests[t].title,
+            click: function(){
+                $("#startTest").bind('click',function(){
+                    location.replace("../html/Stest.html");
+                });
+            }
+        }).appendTo("#testList");
+    }
+
+    //searching...
     $("#myInput").on("keyup",function(){
         var value = $(this).val().toLowerCase();
         $("#testList button").filter(function(){
@@ -51,4 +101,3 @@ function _filter(ClassName,Id_This){
     }
 }
 
-//set data into button
