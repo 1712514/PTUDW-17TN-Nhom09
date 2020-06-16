da7c70f54a768a2ea081c4c43e81c40507d48e30
Rayl99 <1712504@student.hcmus.edu.vn>
2020-06-16 21:48:57 +0700

    finished Stest.html & Stest.js

--- front-end/js/Stest.js
+++ front-end/js/Stest.js
@@ -10,25 +10,25 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 3",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"C"},
   {order:"Câu 4",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"C"},
   {order:"Câu 5",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"B"},
   {order:"Câu 6",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -40,19 +40,19 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 8",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 9",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 10",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -88,19 +88,19 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"C"},
   {order:"Câu 16",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"B"},
   {order:"Câu 17",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"C"},
   {order:"Câu 18",question:" Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -112,31 +112,31 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 20",question:" Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 21",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 22",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 23",question:" Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"C"},
   {order:"Câu 24",question:" Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -148,7 +148,7 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"B"},
   {order:"Câu 26",question:" Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -160,13 +160,13 @@ var questions=[
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"B"},
   {order:"Câu 28",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
   C:"Glucozo hòa tan Cu(OH)2 cho dung dịch phức xanh lam, còn saccarozo thì không có phản ứng này",
   D:"Saccarozo tác dụng H2SO4 đậm đặc đun nóng bị hóa đen, còn glucozo thì không có phản ứng này",
-  answer:"A"},
+  answer:"D"},
   {order:"Câu 29",question:"Chọn phát biểu đúng về sự khác biệt tính chất của glucozo và saccarozo",
   A:"Glucozo có tính chất của 1 anđehit còn saccarozo thì không có tính chất của 1 andehit",
   B:"Saccarozo là đường nho và glucozo là đường mía",
@@ -241,13 +241,20 @@ var questions=[
   answer:"A"},
 ];
 
-var endTime = new Date().getTime() + 5000;//5400000;
+var endTime = new Date().getTime() + 5400000;//90'
 var x = setInterval(function() {
     // Get today's date and time
     var now = new Date().getTime();
     
     if(now > endTime){
-      confirm("Đã hết thời gian làm bài!");
+      $("#myModal").modal('show');
+      $("#cancel").remove();
+      $("#theEnd").text("Xem kết quả");
+      $("#notification").text("Rất tiếc! Thời gian làm bài của bạn đã hết.");
+      $("#theEnd").bind('click',function(){
+        showResult();
+      });
+      clearInterval(x);
     }
     else{
       // Find the distance between now and the count down date
@@ -271,9 +278,15 @@ var x = setInterval(function() {
   }, 1000);
 
 
+window.addEventListener("beforeunload", function(event) {
+    event.returnValue = "Bài thi chưa hoàn thành! Bạn vẫn muốn rời đi chứ?";
+  });
+
 $(document).ready(function(){
 
     for(var i = 0;i<questions.length;i++){
+
+      localStorage.setItem("cau"+i.toString(),null);
 
       if(i == 0){
         $("<a/>",{"class":"nav-link active col-2","id":"sec0","href":"#section" + i.toString(),text:questions[i].order}).appendTo("#tb_question");
@@ -305,36 +318,83 @@ $(document).ready(function(){
         $("<input/>",{"class":"form-check-input","id":"form-check-input_"+c.toString()+i.toString(),"type":"radio","name":"optradio","value":String.fromCharCode(c)}).appendTo("#form_check_label_"+i.toString()+c.toString());
         switch(c) {
           case 65:
-            $("<p/>",{"id":String.fromCharCode(c),text:questions[i].A}).appendTo("#form_check_label_"+i.toString()+c.toString());
+            $("<p/>",{"id":c.toString()+i.toString(),text:questions[i].A}).appendTo("#form_check_label_"+i.toString()+c.toString());
             break;
           case 66:
-            $("<p/>",{"id":String.fromCharCode(c),text:questions[i].B}).appendTo("#form_check_label_"+i.toString()+c.toString());
+            $("<p/>",{"id":c.toString()+i.toString(),text:questions[i].B}).appendTo("#form_check_label_"+i.toString()+c.toString());
             break;
           case 67:
-            $("<p/>",{"id":String.fromCharCode(c),text:questions[i].C}).appendTo("#form_check_label_"+i.toString()+c.toString());
+            $("<p/>",{"id":c.toString()+i.toString(),text:questions[i].C}).appendTo("#form_check_label_"+i.toString()+c.toString());
             break;
           case 68:
-            $("<p/>",{"id":String.fromCharCode(c),text:questions[i].D}).appendTo("#form_check_label_"+i.toString()+c.toString());
+            $("<p/>",{"id":c.toString()+i.toString(),text:questions[i].D}).appendTo("#form_check_label_"+i.toString()+c.toString());
             break;
         }
       }
       $("<div/>",{"class":"col-2 image","id":"image_"+i.toString()}).appendTo("#s01_"+i.toString());
       $("<img/>",{"class":"image-test","src":"../resource/homepage-review.jpg","alt":"abc"}).appendTo("#image_"+i.toString())
+      if(i == questions.length-1){
+        $("<div/>",{"class":"emptySpace"}).appendTo("#section"+i.toString());
+      }
     }
+
 
     var radio = document.getElementsByClassName("form-check-input");
     for(var j = 0;j<radio.length;j++){
       radio[j].addEventListener("change", function () {
         var id = this.getAttribute("id");
       if($("#" + id).is(":checked")){
-        $("#sec"+id.slice(19,id.length)).addClass("picked");
+        
+        $("#sec"+id.slice(19,id.length)).css("background-color", "#99FF33");
+        localStorage.setItem("cau" + id.slice(19,id.length),id.slice(17,19));
       }
     });
     }
 
     $("#Submit").click(function(){
       $("#theEnd").bind('click',function(){
-        location.replace("../html/login.html");
+        showResult();
     });
     });
 });
+
+
+function showResult(){
+  $("#timer").remove();
+  $("#Submit").remove();
+  var numberOfCorrect = questions.length;;
+  $("input:radio[name=optradio]").attr("disabled",true);
+  for(var i = 0;i<questions.length;i++){
+    if(questions[i].answer != String.fromCharCode(parseInt(localStorage.getItem("cau" + i.toString())))){
+      $("#sec" + i.toString()).css("background-color", "red");
+      numberOfCorrect--;
+      if(localStorage.getItem("cau" + i.toString()) != null){
+        $("#" + localStorage.getItem("cau" + i.toString()) + i.toString()).css("color","red");
+      }
+      $("#" + questions[i].answer.charCodeAt(0).toString() + i.toString()).css("color","#CCFF00");
+    }
+    else{
+      $("#" + localStorage.getItem("cau" + i.toString()) + i.toString()).css("color","#CCFF00");
+    }
+
+    $("<div/>",{"class":"hint","id":"hint_" + i.toString()}).appendTo("#section"+i.toString());
+    $("<div/>",{"class":"flip-card","id":"hintCard_" + i.toString()}).appendTo("#hint_"+i.toString());
+    $("<div/>",{"class":"card-front","id":"front_"+i.toString(),text:"Nhấn để xem gợi ý bài giải"}).appendTo("#hintCard_" + i.toString());
+    $("<div/>",{"class":"card-back","id":"back_"+i.toString(),text:"Ở đây chứa gợi ý bài giải! Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS, để tiến hành đàm phán phân định ranh giới trên biển với Trung Quốc hoặc về bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế.Bởi vậy, Indonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế, đặc biệt là UNCLOS ndonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế ndonesia nhận thấy không có lý do pháp lý nào theo luật quốc tế  bất kỳ vấn đề nào khác liên quan đến quyền lợi hoặc lợi ích hàng hải được đưa ra trái với luật quốc tế."}).appendTo("#hintCard_" + i.toString());
+
+    $("#hintCard_"+i.toString()).click(function(){
+      $(this).toggleClass("is-flipped");
+    });
+
+    $("#section"+i.toString()).css("height","800px");
+
+  }
+
+  var point = numberOfCorrect*(10/questions.length);
+
+  $("#point").text(point.toString());
+  $("#numberOfCorrect").text("Số câu đúng: " + numberOfCorrect.toString() + " / " + questions.length.toString());
+  
+}
+
+
