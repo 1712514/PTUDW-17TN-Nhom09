fc4201d6c75155780f63866ec07784dffe415807
1712694 <1712694@student.hcmus.edu.vn>
2020-07-15 14:58:59 +0700

    push Sprofile

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/student/Shomepage', function(req, res, next) {
+  res.render('student/Shomepage', { title: 'Trang chủ', layout:'student/student-layout' });
 });
-
+router.get('/student/Sprofile', function(req, res, next) {
+  res.render('student/Sprofile', { title: 'Thông tin', layout:'student/student-layout' });
+});
 module.exports = router;
