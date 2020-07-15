a49c13531619e6176a830b0653eb285f0256d3b1
1712694 <1712694@student.hcmus.edu.vn>
2020-07-15 14:27:59 +0700

    add pages: Shomepage, login, signup

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,12 +1,12 @@
 login_func = () => {
     var input = document.getElementById("username-input").value;
     if (input == "student"){
-        document.getElementById("btn-login-signup").href="../html/Shomepage.html";
-        window.location.href = "../html/Shomepage.html";
+        document.getElementById("btn-login-signup").href="/users/student/Shomepage";
+        window.location.href = "/users/student/Shomepage";
     }
     else if (input == "teacher"){
-        document.getElementById("btn-login-signup").href="../html/Thomepage.html";
-        window.location.href = "../html/Thomepage.html";
+        document.getElementById("btn-login-signup").href="/users/teacher/Thomepage";
+        window.location.href = "/users/teacher/Thomepage";
     }
     else {
         alert("Sai tên đăng nhập hoặc mật khẩu!!!");
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Đăng nhập' });
 });
-
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Đăng nhập' });
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Trang chủ' });
+});
 module.exports = router;
