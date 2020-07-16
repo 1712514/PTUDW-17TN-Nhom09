ec910a13025235e9547ab2df4bc5a7d911f7882a
Rayl99 <1712504@student.hcmus.edu.vn>
2020-07-16 20:37:57 +0700

    add Ssearchreview.hbs, Ssearchtest.hbs & Stest.hbs

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,15 @@ router.get('/student/Shomepage', function(req, res, next) {
 router.get('/student/Sprofile', function(req, res, next) {
   res.render('student/Sprofile', { title: 'Thông tin', layout:'student/student-layout' });
 });
-
+router.get('/student/Ssearchreview', function(req, res, next) {
+  res.render('student/Ssearchreview', { title: 'Luyện chuyên đề', layout:'student/student-layout' });
+});
+router.get('/student/Ssearchtest', function(req, res, next) {
+  res.render('student/Ssearchtest', { title: 'Luyện đề', layout:'student/student-layout' });
+});
+router.get('/student/Stest', function(req, res, next) {
+  res.render('student/Stest', { title: 'Luyện đề', layout:'student/student-layout' });
+});
 // Teacher
 router.get('/teacher/Thomepage', function(req, res, next) {
   res.render('teacher/Thomepage', { title: 'Trang chủ', layout:'teacher/teacher-layout' });
